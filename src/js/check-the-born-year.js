56ae1dc73639bd9de17ee676e932d2032015d5b3
34x340 <114092241+34x340@users.noreply.github.com>
2024-03-23 00:10:33 +0200

    fix

--- src/js/check-the-born-year.js
+++ src/js/check-the-born-year.js
@@ -1,7 +1,6 @@
 document.querySelector('.input-but__leap').addEventListener('click', function() {
     let userInput = document.getElementById('leap-input').value;
 
-    // Перевірка чи введене число - "29.02"
     if (userInput === "29.02") {
         document.getElementById('yes-no-leap').textContent = "Ви народились у високосний рік";
     } else {
