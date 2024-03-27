15d570f60166ed877c6876f919b6c74f98f04cce
34x340 <114092241+34x340@users.noreply.github.com>
2024-03-27 19:53:09 +0200

    bugs

--- src/js/check-the-born-year.js
+++ src/js/check-the-born-year.js
@@ -1,10 +1,12 @@
 document.querySelector('.input-but__leap').addEventListener('click', function() {
     let userInput = document.getElementById('leap-input').value;
 
-    // Перевірка чи введене число - "29.02"
-    if (userInput === "29.02") {
+
+    if (userInput) {
+        userInput.value % 4 === 0
         document.getElementById('yes-no-leap').textContent = "Ви народились у високосний рік";
     } else {
         document.getElementById('yes-no-leap').textContent = "Ви не народились у високосний рік";
     }
 });
+
