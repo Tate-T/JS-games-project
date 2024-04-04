4fe43a2c05563deea63cf960714f4421147f2048
Mokalol25 <marko.zhoba@gmail.com>
2024-04-04 19:02:49 +0300

    fix

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,26 +1,48 @@
-// const gmail = document.querySelector(".footer__gmail-input").value
-// if (gmail.includes("@gmaii.com")) {
-  const footerInput = document.querySelector(".footer__gmail-input")
-  const refs = {
-    openModalBtn: document.querySelector('[data-open-modal]'),
-    closeModalBtn: document.querySelector('[data-close-modal]'),
-    backdrop: document.querySelector('[data-backdrop]'),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+  footerInput: document.querySelector(".footer__gmail-input"),
+  backdropButton: document.querySelector(".footer__subscribe"),
+};
+function toggleModal() {
+  refs.backdrop.classList.toggle('footer__is-hidden');
+  footerInput.value = ""
+}
+
+function logBackdropClick() {
+  console.log('Це клік в бекдроп');
+}
+refs.backdropButton.addEventListener("click", (event) => {
+  if (refs.footerInput.value === "") {
+    alert("Введіть пошту")
+  } else {
   
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-  refs.backdrop.addEventListener('click', logBackdropClick);
-  
-  function toggleModal() {
-    refs.backdrop.classList.toggle('footer__is-hidden');
-    footerInput.value = ""
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    
+    refs.backdrop.addEventListener('click', logBackdropClick);
+    
   }
+})
+//     const refs = {
+//       openModalBtn: document.querySelector('[data-open-modal]'),
+//       closeModalBtn: document.querySelector('[data-close-modal]'),
+//       backdrop: document.querySelector('[data-backdrop]'),
+//     };
   
-  function logBackdropClick() {
-    console.log('Це клік в бекдроп');
-  }
-// } else {
-//   alert("ds")
-// }
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
+    
+//     refs.backdrop.addEventListener('click', logBackdropClick);
+    
+//     function toggleModal() {
+//       refs.backdrop.classList.toggle('footer__is-hidden');
+//       footerInput.value = ""
+//     }
+    
+//     function logBackdropClick() {
+//       console.log('Це клік в бекдроп');
+//     }
+
   
